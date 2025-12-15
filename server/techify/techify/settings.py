aab2d14db1f00fa16a3174f3ace676a9ffa1b714
Davis-don <davismugoikou@gmail.com>
2025-12-15 19:54:43 +0300

    added  configs

--- server/techify/techify/settings.py
+++ server/techify/techify/settings.py
@@ -154,7 +154,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://kinstry-systems-client.onrender.com"
+    "https://kinstry-systems-client-39w8.onrender.com",
+    "http://kinstryx.co.ke"
 ]
 
 
